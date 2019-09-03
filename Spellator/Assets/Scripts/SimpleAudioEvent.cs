5fde6e8f9ded3bebdc65ac88b30b6db4d02a0f33
Nathan Pana <nathanpana@gmail.com>
2019-09-03 22:44:36 +0930

    Updated editor scripts to allow for proper build

--- Spellator/Assets/Scripts/SimpleAudioEvent.cs
+++ Spellator/Assets/Scripts/SimpleAudioEvent.cs
@@ -6,17 +6,19 @@ public class SimpleAudioEvent : AudioEvent
 {
     public AudioClip[] clips;
 
-    public RangedFloat volume;
+    //public RangedFloat volume;
 
-    [MinMaxRange(1, 2)]
+    //[MinMaxRange(1, 2)]
     //public RangedFloat pitch;
+    public float maxVolume;
+    public float minVolume;
 
     public override void Play(AudioSource source)
     {
         if (clips.Length == 0) return;
 
         source.clip = clips[Random.Range(0, clips.Length)];
-        source.volume = Random.Range(volume.minValue, volume.maxValue);
+        source.volume = Random.Range(minVolume, maxVolume);
       //  source.pitch = Random.Range(pitch.minValue, pitch.maxValue);
         source.Play();
     }
