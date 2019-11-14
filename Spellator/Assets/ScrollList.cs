8bfa22d249e067a7568806aeadd534c6b0586256
Nathan Pana <nathanpana@gmail.com>
2019-11-14 22:43:24 +1030

    Added options for choosing list children

--- Spellator/Assets/ScrollList.cs
+++ Spellator/Assets/ScrollList.cs
@@ -1,38 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScrollList : MonoBehaviour
 {
 
-    public string objectTag;
+
+    [Header("Scriptable objects")]
     private GameObject listObject;
     public ConfigSO configObject;
     public Transform listTransform;
-    [SerializeField] private GameObject closeButton;
+
+    public string listType;
+
+
+    [Header("List variables")]
+    public string listTitle;
+    public string subtitle;
     public int listLength;
-    public bool closeButtonActive = false;
+
+
+    [Header("Text boxes")]
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI subtitleText;
+
+    public static TextMeshProUGUI wordText;
+
+
+
+    [SerializeField] private GameObject closeButton;
+    public bool closeButtonActive;
 
 
     // Start is called before the first frame update
     void Start()
     {
+       
+
+        switch (listType)
+        {
+            case "word":
+                Debug.Log("enum type: word");
+
+
+                for (int i = 0; i < configObject.uniqueWordsList.Count; i++)
+                {
+                    listObject = ObjectPooler.SharedInstance.GetPooledObject("Word List");
+                    if (listObject != null)
+                    {
+
+                        listObject.transform.position = gameObject.transform.position;
+                        listObject.transform.SetParent(listTransform.transform);
+                        //set the value of the text box in the prefab to be the word from the list
+                        //listObject.GetComponent<TextMeshProUGUI>().text = configObject.uniqueWordsList[i];
+                        listObject.SetActive(true);
+                        
+
+                    }
+                }
+
+
+                break;
+
+            case "achievement":
+                Debug.Log("enum type: achievement");
+
+                for (int i = 0; i < listLength; i++)
+                {
+                    listObject = ObjectPooler.SharedInstance.GetPooledObject("Achievement");
+                    if (listObject != null)
+                    {
+
+                        listObject.transform.position = gameObject.transform.position;
+                        listObject.transform.SetParent(listTransform.transform);
+                        //set the value of the text box in the prefab to be the word from the list
+                        listObject.SetActive(true);
+
+                    }
+                }
+
+                break;
+        }
+
+
+        //set the list title and subtitle
+        titleText.text = listTitle;
+        subtitleText.text = subtitle;
+
         //set the close button to active or inactive
         closeButton.SetActive(closeButtonActive);
 
-        for (int i = 0; i < listLength; i++)
-        {
-            listObject = ObjectPooler.SharedInstance.GetPooledObject(objectTag);
-            if (listObject != null)
-            {
-
-                listObject.transform.position = gameObject.transform.position;
-                listObject.transform.SetParent(listTransform.transform);
-                //set the value of the text box in the prefab to be the word from the list
-                listObject.SetActive(true);
-
-            }
-        }
+        
     }
 
     public void HideScrollList()
