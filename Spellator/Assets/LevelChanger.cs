80344973faffef929af30ec2117c26791a942f44
Nathan Pana <nathanpana@gmail.com>
2020-01-11 22:39:31 +1030

    Added fade transitions

---
+++ Spellator/Assets/LevelChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+        }*/
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        animator.SetTrigger("FadeOut");
+        Debug.Log("I'm faded" );
+        levelToLoad = levelIndex;
+
+
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
