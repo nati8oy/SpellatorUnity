d2ae33edb380e37479a6c01853a8da360255ecee
Nathan iMac <nathanpana@gmail.com>
2020-07-22 11:38:31 +0930

    Additional tutorial added

---
+++ Spellator/Assets/TutorialSlider.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialSlider : MonoBehaviour
+{
+
+    private Vector3 sliderPanelPosition;
+    public GameObject sliderPanel;
+    public GameObject backButton;
+    public GameObject nextButton;
+    public GameObject playButton;
+
+    public int slideNumber;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slideNumber = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slideNumber == 1)
+        {
+            backButton.SetActive(false);
+        }
+        else
+        {
+            backButton.SetActive(true);
+        }
+
+        if (slideNumber == 6)
+        {
+            nextButton.SetActive(false);
+            playButton.SetActive(true);
+        }
+        else if (slideNumber < 6)
+        {
+            nextButton.SetActive(true);
+            playButton.SetActive(false);
+        }
+
+       
+    }
+
+
+        public void Slide(string direction)
+    {
+      
+        if (direction == "next")
+        {
+            iTween.MoveBy(sliderPanel, new Vector3(-640, 0, 0), 0.2f);
+            slideNumber += 1;
+        }
+         else if(slideNumber>1)
+        {
+            iTween.MoveBy(sliderPanel, new Vector3(640, 0, 0), 0.2f);
+            slideNumber -= 1;
+        }
+
+    }
+
+    public void HideTutorial()
+    {
+        gameObject.SetActive(false);
+    }
+}
