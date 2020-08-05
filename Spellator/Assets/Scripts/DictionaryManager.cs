9798b18a021cda1727b014b4d0571d509e33be85
Nathan Pana <nathanpana@gmail.com>
2020-08-05 22:01:28 +0930

    Updated app icon

--- Spellator/Assets/Scripts/DictionaryManager.cs
+++ Spellator/Assets/Scripts/DictionaryManager.cs
@@ -1023,6 +1023,7 @@ public class DictionaryManager : MonoBehaviour
 
     }
 
-  
+
+
 
 }
--- Spellator/Assets/Scripts/LevelDescription.cs
+++ Spellator/Assets/Scripts/LevelDescription.cs
@@ -8,6 +8,7 @@ public class LevelDescription : MonoBehaviour
     public TextMeshProUGUI levelRules;
     public LevelManagerSO levelData;
     public TextMeshProUGUI levelNumber;
+    public Transitions fadeManager;
 
     // Update is called once per frame
     void Update()
@@ -17,11 +18,16 @@ public class LevelDescription : MonoBehaviour
 
     }
 
-    public void MoveOffScreen()
+    public void RemoveThis()
     {
+
+        fadeManager = GameObject.Find("Fade Manager").GetComponent<Transitions>();
+        fadeManager.FadeType(fadeManager._flashColour, fadeManager.pulseSpeed);
+        gameObject.SetActive(false);
+
         //iTween.ColorTo(gameObject, iTween.Hash("a", 0f, "time", 3f));
 
-        iTween.MoveTo(gameObject, iTween.Hash("x", -650, "time", 3f, "easetype", "InOutQuad"));
+        //  iTween.MoveTo(gameObject, iTween.Hash("x", -650, "time", 3f, "easetype", "InOutQuad"));
 
     }
 }
